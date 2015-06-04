56e8ca1bb9630289879ed828ae7b0f76444c5b1b
docooler <docooler@njust.edu.cn>
2015-06-04 19:34:21 +0800

    [docooler] add keyboard function

--- src/GameEngine/KeyBoard.java
+++ src/GameEngine/KeyBoard.java
@@ -13,9 +13,29 @@ public class KeyBoard implements IKeyBoard {
 
     All execution stops until a key is pressed, then the value of that key is stored in Vx.*/
 	public byte waitForValue(){
-		byte prveValue = m_value;
-		m_value = (byte)0xff;
-		return prveValue;
+		char inputKey = 0xff;
+		byte value = (byte)0xff;
+		
+		while(inputKey == 0xff){
+			try{
+				inputKey = (char)System.in.read();
+				}catch(Exception e1){
+					e1.printStackTrace();
+				    return (byte)0xff;	
+				}
+			
+			value = convertCharToValue(inputKey);
+		}
+		m_value = value;
+		return value;
+	}
+	
+	private byte convertCharToValue(char key){
+		if(key >= '0' && key <= '9')
+			return (byte)(((int)key) - 48);
+		if(key >= 'a' && key <= 'f')
+			return (byte)(((int)key) - 87);
+		return (byte)0xff;
 	}
 	
 	public void setValue(byte value){
